8ad20de1d55e7248274c16305b25988109c3eb5f
abhii06 <abhishekgupta9332@gmail.com>
2024-03-23 05:26:27 +0530

    Initial commit

---
+++ src/components/Select.jsx
@@ -0,0 +1,35 @@
+import React,{useId} from 'react'
+
+function Select( {
+    label,
+    type= "text",
+    className= "",
+    ...props
+
+}, ref){
+    const id=useId()
+
+  return (
+    <div className='w-full'>
+        {label && <lebel htmlFor={id} className=''></lebel>}
+        <select
+        {...props}
+        id={id}
+        ref={ref}
+        className={`px-3 py-2 rounded-lg bg-white
+        text-black outline-none focus:bg-gray-50
+         duration-200 border border-gray-200 w-full
+        ${className}`}
+        >
+            {options?.map((option) =>(
+              <option key={option} value={option}>
+                {option}
+              </option>
+
+            ))}
+        </select>
+     </div>
+  )
+}
+
+export default React.forwardRef (Select)
